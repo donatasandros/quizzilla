3743d1df418dc5037d4ada32922a5fb86b3bae76
donatas <donatas.andros@gmail.com>
2025-03-06 19:28:59 +0200

    init project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginImport from "eslint-plugin-import";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +14,35 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  eslintConfigPrettier,
+  {
+    plugins: {
+      import: eslintPluginImport,
+    },
+    rules: {
+      "no-shadow": "off",
+      "import/no-cycle": "warn",
+      "import/order": [
+        "error",
+        {
+          "newlines-between": "always",
+          groups: [
+            "builtin",
+            "external",
+            "type",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+          ],
+          alphabetize: {
+            order: "asc",
+          },
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
