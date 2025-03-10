256263fa5265685311184b30943c204a740354cd
donatas <donatas.andros@gmail.com>
2025-03-10 17:46:17 +0200

    feat: setup drizzle

--- src/lib/auth/server/instance.ts
+++ src/lib/auth/server/instance.ts
@@ -1,7 +1,14 @@
 import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+import { db } from "@/drizzle/db";
+import { env } from "@/env";
 
 export const authServer = betterAuth({
   appName: "Quizzilla",
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
@@ -9,8 +16,8 @@ export const authServer = betterAuth({
   },
   socialProviders: {
     github: {
-      clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
     },
   },
   account: {
